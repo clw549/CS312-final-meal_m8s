572248c40f6f5e7c157e0e77f9c598a65c1af752
Ciaran Wham <clw549@nau.edu>
2025-11-11 16:55:34 -0700

    adding basic functionality

--- server/views/login.js
+++ server/views/login.js
@@ -2,8 +2,17 @@ import react from "react"
 
 export function LoginPage()
 {
-  return `<div><form method="post" action="/login">
+  return `<div><form method="post" action="/login">Login
       <label>Username:<input name="username" type="text" /></label>    
       <label>Password<input name="password" type="password"/></label>
     </form></div>`
+}
+
+export function SignupPage() {
+  return `<div>
+    <form method="post" action="/signup">Signup
+      <label>Username:<input type="text" name="username"/></label>
+      <label>Password:<input type="password" name="password"/></label>
+    </form>
+  </div>`
 }
